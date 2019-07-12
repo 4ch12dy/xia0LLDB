74230f01fd1d4050a98b6375dc6c7165b9b7dba9
xia0z <939529983@qq.com>
2019-07-12 18:18:17 +0800

    update install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+shell_root_dir=$(pwd)
+xlldb_file="xlldb.py"
+xlldb_file_path=$shell_root_dir"/"$xlldb_file
+lldbinit=$HOME"/.lldbinit"
+
+sed -i "-bk" '/.*xlldb\.py/d' $lldbinit
+
+if [[ -f $lldbinit ]]; then
+	echo "lldbinit file exist, add xlldb.py to $lldbinit"
+	echo -e "\ncommand script import $xlldb_file_path" >> $lldbinit
+else
+	echo "lldbinit file not exist, add xlldb.py to $lldbinit"
+	echo -e "\ncommand script import $xlldb_file_path" > $lldbinit	
+fi
+
+echo "done."
